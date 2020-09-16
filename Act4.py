8c77753b910795c3662907bf6b3d244d965fa3e1
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-09-15 19:37:01 -0500

    Se incluye la funcion para determinar el signo zodiacal

--- Act4.py
+++ Act4.py
@@ -9,6 +9,68 @@ def area_triangulo(base = int, altura = int)->int:
 def area_circulo(radio = float)->float:
     return (radio*radio)*3.1416
 
-print("El area de un cuadrado con lados de valor 2 es: ", area_cuadrado(2))
-print("El area de un triangulo de base 3 y altura 5 es de: ", area_triangulo(3,5))
-print("El area de un circulo con radio de 4.5 es de: ", area_circulo(4.5))
+def signo_zodiacal(dia, mes):
+    if mes == "1":
+        if dia < "21":
+            print("Eres Capricornio")
+        else:
+            print("Eres Acuario")
+    elif mes == "2":
+        if dia < "20":
+            print("Eres Acuario")
+        else:
+            print("Eres Piscis")
+    elif mes == "3":
+        if dia < "21":
+            print("Eres Piscis")
+        else:
+            print("Eres Aries")
+    elif mes == "4":
+        if dia < "20":
+            print("Eres Aries")
+        else:
+            print("Eres Tauro")    
+    elif mes == "5":
+        if dia < "21":
+            print("Eres Tauro")
+        else:
+            print("Eres Géminis")
+    elif mes == "6":
+        if dia < "22":
+            print("Eres Géminis")
+        else:
+            print("Eres Cáncer")
+    elif mes == "7":
+        if dia < "24":
+            print("Eres Cáncer")
+        else:
+            print("Eres Leo")
+    elif mes == "8":
+        if dia < "24":
+            print("Eres Leo")
+        else:
+            print("Eres Virgo")
+    elif mes == "9":
+        if dia < "23":
+            print("Eres Virgo")
+        else:
+            print("Eres Libra")
+    elif mes == "10":
+        if dia < "23":
+            print("Eres Libra")
+        else:
+            print("Eres Escorpión")
+    elif mes == "11":
+        if dia < "23":
+            print("Eres Escorpión")
+        else:
+            print("Eres Sagitario")
+    elif mes == "12":
+        if dia < "23":
+            print("Eres Sagitario")
+        else:
+            print("Eres Acuario")
+    else:
+        print("Mes no valido")
+
+signo_zodiacal(input("Dia: "),input("Mes: "))
